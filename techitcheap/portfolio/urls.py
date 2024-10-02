2f066bb6904576a1a4d00c181ee120754be2c40a
gois <olatunjihassan22@gmail.com>
2024-10-02 22:37:38 +0100

    PRD deliverables now complete. Portfolio page, Contact Us form and Search Functionality

--- techitcheap/portfolio/urls.py
+++ techitcheap/portfolio/urls.py
@@ -5,6 +5,7 @@ app_name = "portfolio"
 
 urlpatterns = [
     path("", views.portfolio_index, name="portfolio_index"),
+    path('search/', views.search, name='search'),
     path('<int:pk>/', views.project_details, name='project_details'),
     path('category/<str:option>', views.category, name='category')
 ]
--- techitcheap/portfolio/views.py
+++ techitcheap/portfolio/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from portfolio.models import Project
+from .forms import SearchForm
 
 # Create your views here.
+
+
+
 def portfolio_index(request):
+    
     projects = Project.objects.all().order_by("-date")
     context = {
         "projects":projects
@@ -26,3 +31,21 @@ def project_details(request, pk):
     }
     return render(request, "portfolio/project_details.html", context)
 
+def search(request):
+    form = SearchForm(request.GET or None)
+    results = []
+    if form.is_valid():
+        query = form.cleaned_data['query']
+
+        r_title = Project.objects.filter(title__icontains = query)
+        for obj in r_title:
+            results.append(obj)
+        r_category = Project.objects.filter(category__icontains = query)
+        for obj in r_category:
+            results.append(obj)
+        r_desc= Project.objects.filter(description__icontains = query)
+        for obj in r_desc:
+            results.append(obj)
+                        
+    context = {'form':form, 'results':results}
+    return render(request, 'portfolio/search.html', context)
